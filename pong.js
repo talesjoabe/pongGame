4c301b73182497483c9efa3052590dcf56d09a2f
Tales Joabe Lima da Costa <taleslimacosta@gmail.com>
2019-11-25 19:06:29 -0300

    jogo finalizado! falta fazer o controle pelo arduino e a comunicação serial

--- pong.js
+++ pong.js
@@ -2,8 +2,14 @@ p1x=0;
 p2x=0;
 vel=5;
 
+score1=0;
+score2=0;
+
 px_b=75;
 py_b=32;
+
+var bx_speed = -4;
+var by_speed = -4;
 
 
 function setup() {
@@ -13,6 +19,7 @@ function setup() {
 
 function draw() {
   background(bg);
+  Score();
 
   fill(51, 102, 204);
   rect(p1x,0,150,20);
@@ -23,7 +30,13 @@ function draw() {
   fill(255,255,255);
   ellipse(px_b,py_b,25,25);
 
+  moveBall();
+  movePlayers();
+  bounce();
 
+}
+
+function movePlayers(){
   if(p1x>=0 && p1x<=850){
     if(keyIsDown(RIGHT_ARROW)) p1x=p1x+vel;
     else if(keyIsDown(LEFT_ARROW)) p1x=p1x-vel;
@@ -37,5 +50,40 @@ function draw() {
   }
   else if(p2x<=0) p2x=850;
   else p2x=0;
+}
 
+function Score(){
+  fill(255);
+  textSize(24);
+  text(" Jogador 1: " + score1, 850, 20);
+  text("Jogador 2: " + score2, 850, 980);
+}
+
+function moveBall() {
+  px_b += bx_speed;
+  py_b += by_speed;
+}
+
+
+function bounce() {
+  if (px_b < 10 ||
+    px_b > 1000 - 25) {
+    bx_speed *= -1;
+  }
+  if(px_b>=p1x && px_b<= p1x+150 && py_b<=25 ){
+      bx_speed*=-1;
+      by_speed*=-1;
+  }else if(py_b<=0){
+    py_b=p2x+75;
+    px_b=980;
+    score2++;
+  }
+  if(px_b>=p2x && px_b<= p2x+150 && py_b>=985){
+      bx_speed*=-1;
+      by_speed*=-1;
+  } else if(py_b>=1000){
+    px_b=p1x+75;
+    py_b=32;
+    score1++;
+  }
 }
